977e37e65d5aa9b520117f1e78d3166a88b23a36
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-09-05 11:33:17 +0530

    Image issue resolved

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -2,15 +2,15 @@ import "./style.css";
 
 export default function Skills() {
   const skills = [
-    { name: "HTML", img: "/assets/html-logo.png" },
-    { name: "CSS", img: "/assets/css-logo.png" },
-    { name: "Tailwind CSS", img: "/assets/tailwindcss-logo.png" },
-    { name: "Vanilla JS", img: "/assets/js-logo.png" },
-    { name: "React.js", img: "/assets/react-logo.png" },
-    { name: "Java", img: "/assets/java-logo.png" },
-    { name: "Python", img: "/assets/python-logo.png" },
-    { name: "MySQL", img: "/assets/mysql-logo.png" },
-    { name: "DSA", img: "/assets/dsa.png" },
+    { name: "HTML", img: "../assets/html-logo.png" },
+    { name: "CSS", img: "../assets/css-logo.png" },
+    { name: "Tailwind CSS", img: "../assets/tailwindcss-logo.png" },
+    { name: "Vanilla JS", img: "../assets/js-logo.png" },
+    { name: "React.js", img: "../assets/react-logo.png" },
+    { name: "Java", img: "../assets/java-logo.png" },
+    { name: "Python", img: "../assets/python-logo.png" },
+    { name: "MySQL", img: "../assets/mysql-logo.png" },
+    { name: "DSA", img: "../assets/dsa.png" },
   ];
 
   return (
