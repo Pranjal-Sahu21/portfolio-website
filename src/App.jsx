f8119ac27276fdaad96a2333cb78268857642907
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2025-09-09 15:14:15 +0530

    Increased the 3d angle

--- src/App.jsx
+++ src/App.jsx
@@ -90,8 +90,8 @@ export default function App() {
         const centerX = rect.width / 2;
         const centerY = rect.height / 2;
 
-        const rotateX = ((y - centerY) / centerY) * -15;
-        const rotateY = ((x - centerX) / centerX) * 15;
+        const rotateX = ((y - centerY) / centerY) * -20;
+        const rotateY = ((x - centerX) / centerX) * 20;
 
         card.style.transform = `perspective(800px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
         card.style.setProperty("--x", `${x - rect.width / 2}px`);
